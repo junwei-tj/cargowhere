3617a6eabf528a18940c371314aa3788a81f936d
Shen Rui <chongshenrui98@gmail.com>
2021-03-01 13:26:04 +0800

    Added picker, view for favourites

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
+import Carpark from './Carpark';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  header: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+    flexDirection: "row",
+    backgroundColor: "#eee",
+    paddingVertical: 10,
+  },
+  headerText: {
+    fontSize: 20,
+    paddingLeft: 20,
+    textDecorationLine: "underline",
   },
 });
 
 export default function FavouritesScreen() {
+
   return (
     <View style={styles.container}>
-      <Text>Favourites Screen</Text>
+    {/* To add CRUD functionality  */}
+      <View style={styles.header}>
+      <Text style={styles.headerText}>Favourites</Text>
+      </View>
+      {/* Put here for now just to see how the list will look  */}
+      <FlatList data={Array(9).fill(0)} renderItem={() => <Carpark />} /> 
     </View>
   );
 }
--- screens/NearbyScreen.js
+++ screens/NearbyScreen.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import Carpark from './Carpark';
 
 const styles = StyleSheet.create({
@@ -8,13 +9,48 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
     // alignItems: 'center',
   },
+  header: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+    flexDirection: "row",
+    backgroundColor: "#eee",
+  },
+  sortBy: {
+    fontSize: 20,
+    paddingTop: 10,
+    textAlign: "right",
+    flex: 1,
+  },
+  headerText: {
+    fontSize: 20,
+    paddingTop: 10,
+    paddingLeft: 20,
+    textDecorationLine: "underline",
+  },
+  picker:{
+    width:"43%",
+    color:"black",
+  }
 });
 
 export default function NearbyScreen() {
+  const [value, setValue] = useState("key0");
+
   return (
     <View style={styles.container}>
       {/* <Text>Nearby Screen</Text> */}
       {/* Need key extractor also*/}
+      <View style={styles.header}>
+      <Text style={styles.headerText}>Nearby</Text>
+        <Text style={styles.sortBy}>Sort By:</Text>
+        <Picker
+        style={styles.picker}
+        selectedValue={value}
+        onValueChange={(itemValue, itemIndex) => setValue(itemValue)}>
+            <Picker.Item label="Distance" value="key0" />
+            <Picker.Item label="Availability" value="key1" />
+        </Picker>
+      </View>
       <FlatList data={Array(9).fill(0)} renderItem={() => <Carpark />} />
     </View>
   );
